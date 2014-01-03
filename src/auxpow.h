f729180724427092cd450fd8b8419e2919fed75b
sidhujag <sidhujag@hotmail.com>
2014-01-03 08:11:36 -0800

    Revert "Removed MT flag from mingw installer.."
    This reverts commit f08dbd0aa0badc61d99663659a995a672db2d1c3.

--- src/auxpow.h
+++ src/auxpow.h
@@ -69,8 +69,7 @@ int ReadWriteAuxPow(Stream& s, boost::shared_ptr<CAuxPow>& auxpow, int nType, in
 {
     if (nVersion & BLOCK_VERSION_AUXPOW)
     {
-		CAuxPow* newPow = new CAuxPow();
-        auxpow.reset(newPow);
+        auxpow.reset(new CAuxPow());
         return SerReadWrite(s, *auxpow, nType, nVersion, ser_action);
     }
     else
